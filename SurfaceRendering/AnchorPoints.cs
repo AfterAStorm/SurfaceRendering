cf8b9126fe1368f3eb8e4cc758fed7ab222e339a
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 16:57:25 -0600

    Add project files.

---
+++ SurfaceRendering/AnchorPoints.cs
@@ -0,0 +1,40 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public static class AnchorPoints
+        {
+            public const Vector2 TopLeft        = new Vector2(0, 0);
+            public const Vector2 TopMiddle      = new Vector2(0, 0);
+            public const Vector2 TopRight       = new Vector2(0, 0);
+
+            public const Vector2 MiddleLeft     = new Vector2(0, 0);
+            public const Vector2 Center         = new Vector2(0, 0);
+            public const Vector2 MiddleRight    = new Vector2(0, 0);
+
+            public const Vector2 BottomLeft     = new Vector2(0, 0);
+            public const Vector2 BottomMiddle   = new Vector2(0, 0);
+            public const Vector2 BottomRight    = new Vector2(0, 0);
+        }
+    }
+}
---
+++ SurfaceRendering/Location.cs
@@ -0,0 +1,52 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public struct Location
+        {
+            #region # - Properties
+
+            private Vector2 pos;
+            private Vector2 anchor;
+
+            public Vector2 Position => pos;
+            public Vector2 AnchorPoint => anchor;
+
+            #endregion
+
+            #region # - Constructor
+
+            public Location(Vector2 position, Vector2 anchor)
+            {
+                pos = position;
+                anchor = anchor;
+            }
+
+            public Location(float x, float y, Vector2 anchor) : base(new Vector2(x, y), anchor) {}
+
+            public Location(float x, float y) : base(x, y, AnchorPoints.Center) {}
+
+            #endregion
+        }
+    }
+}
