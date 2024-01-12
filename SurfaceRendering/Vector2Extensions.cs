3c4bc837e6ed58ca0b4cfa6e67302e381cadb85d
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 20:30:14 -0600

    Vector2 extensions for convenience

---
+++ SurfaceRendering/Vector2Extensions.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+using static IngameScript.Program;
+
+namespace IngameScript
+{
+    // This template is intended for extension classes. For most purposes you're going to want a normal
+    // utility class.
+    // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
+    internal static class Vector2Extensions
+    {
+        public static Location ToLocation(this Vector2 vector, Vector2? anchor = null)
+        {
+            return new Location(vector, anchor ?? AnchorPoints.TopLeft);
+        }
+    }
+}
