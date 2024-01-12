cdefaa126eedec24a20a2a23e9147c5eada6d0c2
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 21:04:57 -0600

    Fixed locations

--- SurfaceRendering/TestRenderer.cs
+++ SurfaceRendering/TestRenderer.cs
@@ -36,27 +36,27 @@ namespace IngameScript
 
                 utensil.DrawRectangle(new Location(0, 0, AnchorPoints.TopLeft), size);
                 utensil.DrawRectangleBorder(new Location(0, 0, AnchorPoints.TopLeft), size, Color.Tomato);
-                utensil.DrawRectangle(new Location(0, 0, AnchorPoints.TopMiddle), size);
-                utensil.DrawRectangleBorder(new Location(0, 0, AnchorPoints.TopMiddle), size, Color.Tomato);
-                utensil.DrawRectangle(new Location(0, 0, AnchorPoints.TopRight), size);
-                utensil.DrawRectangleBorder(new Location(0, 0, AnchorPoints.TopRight), size, Color.Tomato);
+                utensil.DrawRectangle(new Location(utensil.Width / 2, 0, AnchorPoints.TopMiddle), size);
+                utensil.DrawRectangleBorder(new Location(utensil.Width / 2, 0, AnchorPoints.TopMiddle), size, Color.Tomato);
+                utensil.DrawRectangle(new Location(utensil.Width, 0, AnchorPoints.TopRight), size);
+                utensil.DrawRectangleBorder(new Location(utensil.Width, 0, AnchorPoints.TopRight), size, Color.Tomato);
 
-                utensil.DrawRectangle(new Location(0, 0, AnchorPoints.MiddleLeft), size);
-                utensil.DrawRectangleBorder(new Location(0, 0, AnchorPoints.MiddleLeft), size, Color.Tomato);
-                utensil.DrawRectangle(new Location(0, 0, AnchorPoints.Center), size);
-                utensil.DrawRectangleBorder(new Location(0, 0, AnchorPoints.Center), size, Color.Tomato);
-                utensil.DrawRectangle(new Location(0, 0, AnchorPoints.MiddleRight), size);
-                utensil.DrawRectangleBorder(new Location(0, 0, AnchorPoints.MiddleRight), size, Color.Tomato);
+                utensil.DrawRectangle(new Location(0, utensil.Height / 2, AnchorPoints.MiddleLeft), size);
+                utensil.DrawRectangleBorder(new Location(0, utensil.Height / 2, AnchorPoints.MiddleLeft), size, Color.Tomato);
+                utensil.DrawRectangle(new Location(utensil.Width / 2, utensil.Height / 2, AnchorPoints.Center), size);
+                utensil.DrawRectangleBorder(new Location(utensil.Width / 2, utensil.Height / 2, AnchorPoints.Center), size, Color.Tomato);
+                utensil.DrawRectangle(new Location(utensil.Width, utensil.Height / 2, AnchorPoints.MiddleRight), size);
+                utensil.DrawRectangleBorder(new Location(utensil.Width, utensil.Height / 2, AnchorPoints.MiddleRight), size, Color.Tomato);
 
-                utensil.DrawRectangle(new Location(0, 0, AnchorPoints.BottomLeft), size);
-                utensil.DrawRectangleBorder(new Location(0, 0, AnchorPoints.BottomLeft), size, Color.Tomato);
-                utensil.DrawRectangle(new Location(0, 0, AnchorPoints.BottomMiddle), size);
-                utensil.DrawRectangleBorder(new Location(0, 0, AnchorPoints.BottomMiddle), size, Color.Tomato);
-                utensil.DrawRectangle(new Location(0, 0, AnchorPoints.BottomRight), size);
-                utensil.DrawRectangleBorder(new Location(0, 0, AnchorPoints.BottomRight), size, Color.Tomato);
+                utensil.DrawRectangle(new Location(0, utensil.Height, AnchorPoints.BottomLeft), size);
+                utensil.DrawRectangleBorder(new Location(0, utensil.Height, AnchorPoints.BottomLeft), size, Color.Tomato);
+                utensil.DrawRectangle(new Location(utensil.Width / 2, utensil.Height, AnchorPoints.BottomMiddle), size);
+                utensil.DrawRectangleBorder(new Location(utensil.Width / 2, utensil.Height, AnchorPoints.BottomMiddle), size, Color.Tomato);
+                utensil.DrawRectangle(new Location(utensil.Width, utensil.Height, AnchorPoints.BottomRight), size);
+                utensil.DrawRectangleBorder(new Location(utensil.Width, utensil.Height, AnchorPoints.BottomRight), size, Color.Tomato);
 
-                utensil.DrawScaledText(new Location(utensil.Center) - new Location(0, 50), $"Surface Size: {surface.SurfaceSize.X}x{surface.SurfaceSize.Y}", 1f, Color.White);
-                utensil.DrawScaledText(new Location(utensil.Center) - new Location(0, 30), $"Texture Size: {surface.TextureSize.X}x{surface.TextureSize.Y}", 1f, Color.White);
+                utensil.DrawScaledText(new Location(utensil.Width / 2, 50, AnchorPoints.TopMiddle), $"Surface Size: {surface.SurfaceSize.X}x{surface.SurfaceSize.Y}", 1f, Color.White);
+                utensil.DrawScaledText(new Location(utensil.Width / 2, 70, AnchorPoints.TopMiddle), $"Texture Size: {surface.TextureSize.X}x{surface.TextureSize.Y}", 1f, Color.White);
             }
         }
     }
