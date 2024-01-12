77c4481463329330aecbea446c5fcc2904fc1808
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 20:03:53 -0600

    Location class

--- SurfaceRendering/Location.cs
+++ SurfaceRendering/Location.cs
@@ -26,10 +26,10 @@ namespace IngameScript
         {
             #region # - Properties
 
-            private Vector2 pos;
+            private Vector2 position;
             private Vector2 anchor;
 
-            public Vector2 Position => pos;
+            public Vector2 Position => position;
             public Vector2 AnchorPoint => anchor;
 
             #endregion
@@ -38,13 +38,42 @@ namespace IngameScript
 
             public Location(Vector2 position, Vector2 anchor)
             {
-                pos = position;
-                anchor = anchor;
+                this.position = position;
+                this.anchor = anchor;
             }
 
-            public Location(float x, float y, Vector2 anchor) : base(new Vector2(x, y), anchor) {}
+            public Location(float x, float y, Vector2 anchor) : this(new Vector2(x, y), anchor) { }
 
-            public Location(float x, float y) : base(x, y, AnchorPoints.Center) {}
+            public Location(Vector2 position) : this(position.X, position.Y) { }
+
+            public Location(float x, float y) : this(x, y, AnchorPoints.TopLeft) {}
+
+            #endregion
+
+            #region # - Methods
+
+            public Vector2 ToSpritePosition(Vector2 size)
+            {
+                return new Vector2(
+                    position.X - (size.X * anchor.X),
+                    position.Y - (size.Y * anchor.Y)
+                );
+            }
+
+            #endregion
+
+            #region # - Operators
+
+            public static Location operator +(Location a) => a;
+            public static Location operator -(Location a) => new Location(-a.Position.X, -a.Position.Y, a.AnchorPoint);
+
+            public static Location operator +(Location a, Location b) => new Location(a.Position.X + b.Position.X, a.Position.Y + b.position.Y, a.AnchorPoint);
+            public static Location operator -(Location a, Location b) => a + (-b);
+            public static Location operator *(Location a, Location b) => new Location(a.Position.X * b.Position.X, a.Position.Y * b.position.Y, a.AnchorPoint);
+            public static Location operator /(Location a, Location b) => new Location(a.Position.X / b.Position.X, a.Position.Y / b.position.Y, a.AnchorPoint);
+
+            public static Location operator *(Location a, float b) => new Location(a.Position.X * b, a.Position.Y * b, a.AnchorPoint);
+            public static Location operator /(Location a, float b) => new Location(a.Position.X / b, a.Position.Y / b, a.AnchorPoint);
 
             #endregion
         }
